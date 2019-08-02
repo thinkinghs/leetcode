eba847d9b20ec518f8bafd789eb69ce6d2a27036
thinkinghs <45984317+thinkinghs@users.noreply.github.com>
2019-08-02 14:42:36 +0900

    Update 53maximumSubarray.py

--- 53maximumSubarray.py
+++ 53maximumSubarray.py
@@ -6,8 +6,15 @@ class Solution:
         if not nums:
             return 0
         
-        largest = -2^31 
+        # best practice 보니 int의 lowest value를 넣을 필요 없이, largest에 nums[0] 값을 넣으면 됨
+        # curr은 0부터 시작해야되니 curr=0
+        # nums = [-2] 일 경우에 -2 return 해야 되고, nums[-2, -1] 일때 -1 return 해야 됨
+        
+        # largest = -2^31 
+        # curr = 0
+        largest = nums[0]
         curr = 0
+        
         # curr 
         for i in nums:
             curr += i
@@ -18,3 +25,16 @@ class Solution:
             
         return largest
             
+''' The solution that someone got many stars
+def maxSubArray(self, A):
+    if not A:
+        return 0
+    
+    curSum = maxSum = A[0]
+    # 
+    for num in A[1:]:
+        curSum = max(num, curSum + num)
+        maxSum = max(maxSum, curSum)
+    return maxSum
+
+'''

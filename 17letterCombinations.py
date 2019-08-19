036942f5e73ebaa77ae0a96884efb8255e02d320
thinkinghs <45984317+thinkinghs@users.noreply.github.com>
2019-08-19 17:49:03 +0900

    Update 17letterCombinations.py

--- 17letterCombinations.py
+++ 17letterCombinations.py
@@ -45,3 +45,32 @@ class Solution:
             result.append(reduce(lambda a, b: a+b, each))
         return result
     
+
+class bestSolution:
+    def letterCombination(self, digits):
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+        
+        def backtrack(combination, next_digits):
+            # if there is no more digits to check
+            if len(next_digits) == 0:
+                # the combination is done
+                output.append(combination)
+            # if there are still digits to check
+            else:
+                # iterate over all letters which map the next available digit
+                for letter in phone[next_digits[0]]:
+                    # append the current letter to the combination and append (proceed to the next digits) 문법적으론 안 맞는데, 다음 digit 나아가는 부분을 추가하라 라는 의미로 저자가 쓴 듯 
+                    backtrack(combination + letter, next_digits[1:])
+            
+        # 같은 class 안에 있으면 function 밖에 있는 variable 이용 가능
+        output = []
+        if digits:
+            backtrack("", digits)
+        return output
